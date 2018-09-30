005406037539c7c1fe1d42973699e5d802ea71f3
LahiruTjay <lahiru.tjay@gmail.com>
2018-09-30 10:36:15 +0530

    Updated.

--- authorization-server/src/main/java/com/example/auth/UserDetailServiceImpl.java
+++ authorization-server/src/main/java/com/example/auth/UserDetailServiceImpl.java
@@ -55,16 +55,13 @@ public class UserDetailServiceImpl implements UserDetailsService {
     public Set<GrantedAuthority> getGrantedAuthoritiesForUser(SystemUser user) {
 
         Set<GrantedAuthority> grantedAuthoritySet = new HashSet<>();
-        String userRole = user.getRole()
-            .getRoleName();
+        String userRole = user.getRole().getRoleName();
 
         if (userRole != null) {
             Role role = roleRepository.findByRoleName(userRole);
             if (role != null) {
-                roleAuthorityRepository.findAllByRole(role)
-                    .forEach(roleAuthority -> {
-                        grantedAuthoritySet.add(new SimpleGrantedAuthority(roleAuthority.getAuthority()
-                            .getAuthority()));
+                roleAuthorityRepository.findAllByRole(role).forEach(roleAuthority -> {
+                        grantedAuthoritySet.add(new SimpleGrantedAuthority(roleAuthority.getAuthority().getAuthority()));
                     });
                 return grantedAuthoritySet;
             } else {
